1a36dd337f516575cf4bb584c12e141f85b77859
Cesar Santana <casc.santana@gmail.com>
2018-05-01 01:24:32 -0500

    Beautifying routes with vue-routisan

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,25 @@
+/*
+* Using this package https://github.com/raniesantos/vue-routisan
+* to beautify routes. For example
+* {
+*   path: '/',
+*   component: () => import('layouts/default'),
+*   children: [
+*     { path: '', component: () => import('pages/index') }
+*   ]
+* }
+*
+*/
 
-export default [
-  {
-    path: '/',
-    component: () => import('layouts/default'),
-    children: [
-      { path: '', component: () => import('pages/index') }
-    ]
-  },
+import Route from 'vue-routisan'
+// Define path where your views are stored
+Route.setViewResolver(component => require('src/pages/' + component).default)
 
-  { // Always leave this as last one
-    path: '*',
-    component: () => import('pages/404')
-  }
-]
+Route.view('/', 'layouts/default')
+  .children(() => {
+    Route.view('/', 'index')
+  })
+
+Route.view('*', '404')
+
+export default Route.all()
