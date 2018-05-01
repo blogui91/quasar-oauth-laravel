307f3bdf5bd698f675156c1a501cf40e76fadd77
Cesar Santana <casc.santana@gmail.com>
2018-05-01 08:37:36 -0500

    Bug fix, remove duplicated quotes in env values.

--- config/helpers/env.js
+++ config/helpers/env.js
@@ -1,3 +1,7 @@
 module.exports = function (key, fallback) {
-  return process.env[key] || fallback
+  let value = process.env[key]
+  if (!value) {
+    return fallback
+  }
+  return value.replace(/["]/g, '')
 }
