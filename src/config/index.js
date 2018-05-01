928f745a9a958a38c6639f20ab7ebbf976d95d77
Cesar Santana <casc.santana@gmail.com>
2018-05-01 01:13:49 -0500

    removing unncessary code from config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,12 @@
 import { findValue } from 'src/utils'
 import api from './api'
 import auth from './auth'
-import database from './database'
 // import newconfig from './newconfigfile'
 
 export default function (value = '', fallback = null) {
   const values = {
     api,
-    auth,
-    database
+    auth
     // newconfig
   }
   return findValue(value, values) || fallback
